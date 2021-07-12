a726c48905a205b5d0431bc811089dbbfdaa9c69
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2021-07-12 12:27:47 +0700

    first commit

---
+++ src/tests/pages/index.test.tsx
@@ -0,0 +1,12 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render, screen } from '@testing-library/react';
+import Home from '../../pages/index';
+
+describe('Home', () => {
+  it('renders without crashing', () => {
+    render(<Home />);
+    expect(screen.getByRole('heading')).toHaveTextContent('Hello world');
+  });
+});
